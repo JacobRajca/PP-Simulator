fb27b86e1c56af2fca6d032cf3215217af070c0a
JacobRajca <jakubrca@gmail.com>
2024-12-12 16:38:56 +0100

    Settery

--- Animals.cs
+++ Animals.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Simulator;
 
 public class Animals
 {
-    public required string Description { get; init; }
-    public uint Size { get; set; } = 3;
+    private string _description = "Description";
 
+    public string Description
+    {
+        get => _description;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                value = "###";
+            }
+
+            value = value.Trim();
+
+
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }
+
+            if (value.Length > 15)
+            {
+                value = value.Substring(0, 15).TrimEnd();
+            }
+
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }
+
+            if (char.IsLower(value[0]))
+            {
+                value = char.ToUpper(value[0]) + value.Substring(1);
+            }
+
+            _description = value;
+        }
+    }
+
+    public uint Size { get; set; } = 3;
     public string Info => $"{Description} <{Size}>";
 }
 
--- Creature.cs
+++ Creature.cs
@@ -7,26 +7,83 @@ using System.Threading.Tasks;
 namespace Simulator;
 public class Creature
 {
-    public string Name { get; set; }
-    public int Level { get; set; }
+    private string _name = "Unknown";
+    private int _level = 1;
 
-    public Creature(string name, int level=1)
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (!string.IsNullOrEmpty(_name) && _name != "Unknown")
+            {
+                throw new InvalidOperationException("Name can only be set once.");
+            }
+
+            if(string.IsNullOrEmpty(value) )
+            {
+                value = "###";
+            }
+
+            value = value.Trim();
+       
+
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }
+
+            if (value.Length > 25)
+            {
+                value = value.Substring(0, 25).TrimEnd();
+            }
+
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }
+
+            if (char.IsLower(value[0])) 
+            {
+                value = char.ToUpper(value[0]) + value.Substring(1);
+            }
+
+            _name = value;
+        }
+    }
+
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            if(_level != 1)
+            {
+                throw new InvalidOperationException("Level can only be set once.");
+            }
+            _level = Math.Clamp(value, 1,10);
+        }
+    }
+
+    public void Upgrade()
+    {
+        if (_level < 10)
+        {
+            _level++;
+        }
+    }
+    public string Info => $"{Name}, Level {Level}";
+    public Creature(string name, int level = 1)
     {
         Name = name;
         Level = level;
     }
 
-    public Creature() 
-    {
-        Name = "";
-        Level = 1;
-    }
+    public Creature() {}
 
-    public string Info => $"{Name}, Level {Level}";
-
-    public void sayHi()
+    public void SayHi()
     {
-        Console.WriteLine($"Hi, my name is {Name} and I am at level {Level}";
+        Console.WriteLine($"Hi, my name is {Name} and I am at level {Level}");
     }
 }
 
