23674eaa1d6d0a8b3cf57a4d2ed2a0d0d18aa00d
JacobRajca <jakubrca@gmail.com>
2024-12-12 16:03:33 +0100

    Stwory

---
+++ Creature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature(string name, int level=1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() 
+    {
+        Name = "";
+        Level = 1;
+    }
+
+    public string Info => $"{Name}, Level {Level}";
+
+    public void sayHi()
+    {
+        Console.WriteLine($"Hi, my name is {Name} and I am at level {Level}";
+    }
+}
+
